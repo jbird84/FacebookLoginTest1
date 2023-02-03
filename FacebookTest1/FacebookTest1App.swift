33a17ddc2726e472a684c135313dd46e5d2445ae
DoodMac <48062579+jbird84@users.noreply.github.com>
2023-02-03 16:36:57 -0600

    Initial Commit

---
+++ FacebookTest1/FacebookTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookTest1App.swift
+//  FacebookTest1
+//
+//  Created by Kinney Kare on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct FacebookTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
